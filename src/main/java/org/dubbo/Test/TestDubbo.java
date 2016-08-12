966a42861b6ef7605041ddc7eb6d927e2e153dcb
hanxiaoy <hx1653@sina.com>
2016-08-12 17:54:13 +0800

    init dubbo_provider

---
+++ src/main/java/org/dubbo/Test/TestDubbo.java
@@ -0,0 +1,18 @@
+package org.dubbo.Test;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by hanxy on 2016/8/12.
+ * projectName : dubbo-provider
+ * description :
+ */
+public class TestDubbo {
+
+    public static void main(String[] args) throws  Exception{
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "applicationContext.xml" });
+        context.start();
+        System.out.println("按任意键退出");
+        System.in.read();
+    }
+}
